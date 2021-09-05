710386890d3af97ae525539b72945adfb25eeb30
mikep2468 <mike.preece@cinch.co.uk>
2021-09-05 23:12:00 +0100

    add packack-lock.json

--- bin/mybucketapp.ts
+++ bin/mybucketapp.ts
@@ -2,20 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { MybucketappStack } from '../lib/mybucketapp-stack';
+import { PipelineStack} from '../lib/mybucketapp-pipeline-stack';
 
 const app = new cdk.App();
 new MybucketappStack(app, 'MybucketappStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+  env: { account: '375064697969', region: 'eu-west-2' }, //deploy only to my personal account, London region
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+new PipelineStack(app, 'PipelineStack', {
+  env: { account: '375064697969', region: 'eu-west-2' }, //deploy only to my personal account, London region
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/mybucketapp-stack.ts
+++ lib/mybucketapp-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class MybucketappStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    
+    new s3.Bucket(this, 'mybucketapp-bucket-1-05092021', {
+      bucketName: 'mybucketapp-bucket-1-05092021',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: false,
+    });
+
+    new s3.Bucket(this, 'mybucketapp-bucket-2-05092021', {
+      bucketName: 'mybucketapp-bucket-2-05092021',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: false,
+    });
   }
 }
